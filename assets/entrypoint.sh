d429aeb95cff7470deb0229f96a3cbe8f2f21cc5
Mike Cronce <mike@quadra-tec.net>
2017-08-11 22:49:36 -0400

    assets/entrypoint.sh:  Munge server.xml as well

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
 sed -i --follow-symlinks \
+	-e "s/%BASE_PORT%/${TOMCAT_BASE_PORT:-8005}/" \
+	-e "s/%HTTP_PORT%/${TOMCAT_HTTP_PORT:-8080}/" \
+'/usr/local/tomcat/conf/server.xml';
+
+sed -i --follow-symlinks \
 	-e "s/%PLATFORM_HOST%/${PLATFORM_HOST:-alfresco}/" \
 	-e "s/%PLATFORM_PORT%/${PLATFORM_PORT:-8080}/" \
 '/opt/solr/workspace-SpacesStore/conf/solrcore.properties';
