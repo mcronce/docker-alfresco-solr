eeffbffdfd3eb448ec6da68eae5355a6d2a8afa0
Mike Cronce <mike@quadra-tec.net>
2017-08-11 21:30:32 -0400

    assets/entrypoint.sh:  Whoops, should probably munge the correct files

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
 sed -i --follow-symlinks \
-	-e "s/%BASE_PORT%/${TOMCAT_BASE_PORT:-8005}/" \
-	-e "s/%HTTP_PORT%/${TOMCAT_HTTP_PORT:-8080}/" \
-'/usr/local/tomcat/conf/server.xml';
+	-e "s/%PLATFORM_HOST%/${PLATFORM_HOST:-alfresco}/" \
+	-e "s/%PLATFORM_PORT%/${PLATFORM_PORT:-8080}/" \
+'/opt/solr/workspace-SpacesStore/conf/solrcore.properties';
 
 sed -i --follow-symlinks \
 	-e "s/%PLATFORM_HOST%/${PLATFORM_HOST:-alfresco}/" \
 	-e "s/%PLATFORM_PORT%/${PLATFORM_PORT:-8080}/" \
-'/usr/local/tomcat/shared/classes/alfresco/web-extension/share-config-custom.xml';
+'/opt/solr/archive-SpacesStore/conf/solrcore.properties';
 
 /usr/local/tomcat/bin/catalina.sh $@;
 
